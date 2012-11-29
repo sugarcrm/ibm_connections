eeb1ce5334f3a78710eacd6d5fe2794b753eb102
Charles Hicks <carl.hicks@gmail.com>
2012-11-29 10:20:53 -0800

    Blank out connection string

--- SugarModules/custom/modules/Connectors/connectors/sources/ext/eapm/connections/config.php
+++ SugarModules/custom/modules/Connectors/connectors/sources/ext/eapm/connections/config.php
@@ -4,4 +4,4 @@ $config['name'] = 'IBM Connections&#169;';
 $config['eapm']['enabled'] = true;
 $config['eapm']['only'] = true;
 $config['order'] = 14;
-//$config['properties']['company_url'] = 'https://greenhouse.lotus.com';
+$config['properties']['company_url'] = '';
