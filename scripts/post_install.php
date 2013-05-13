90f6bb040e2120212e042c17584780e5e78c67f7
Yuri Kuznetsov <yurak@list.ru>
2013-05-13 12:13:59 +0300

    Enable some modules automaticallu post-install

--- scripts/post_install.php
+++ scripts/post_install.php
@@ -6,6 +6,20 @@ require_once('modules/Administration/QuickRepairAndRebuild.php');
 function post_install() {
    $randc = new RepairAndClear();
    $randc->repairAndClearAll(array('clearAll'),array(translate('LBL_ALL_MODULES')), false, false);
+   
+	$fileNameBase = 'modules/Connectors/metadata/display_config.php';
+	$fileName = get_custom_file_if_exists($fileNameBase);
+	include($fileName);
+	$fileName = "custom/" . $fileNameBase;
+	$moduleList = array(
+		'Accounts',
+		'Opportunities',
+		'Cases',
+	);
+	foreach ($moduleList as $moduleName) {
+		$modules_sources[$moduleName]['ext_eapm_connections'] = 'ext_eapm_connections';
+	}
+	write_array_to_file('modules_sources', $modules_sources, $fileName);
 }
 
 ?>
